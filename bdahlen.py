c18c67d2022bc32f5246a872ed16ae25e0fadf56
gsalinasphys <gsalinas.phys@gmail.com>
2021-04-02 18:58:28 -0400

    Added first 1d cuts

--- bdahlen.py
+++ bdahlen.py
@@ -1,11 +1,20 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from scipy.optimize import root
 from functools import partial
 import numdifftools as nd
 from cosmoTransitions import pathDeformation as pd
 
+import sys, os
+
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
 class Potential:
-    def __init__(self, a = 50., E = 0., epsilon = .01, nmax = 5):
+    def __init__(self, a = 50., E = 0., epsilon = .1, nmax = 5):
         self.params = a, E, epsilon, nmax
 
     def V(self, X):
@@ -29,7 +38,7 @@ class Potential:
     def plotContour(self):
         nx = 100
         nmax = self.params[-1]
-        xmin, xmax, ymin, ymax = 0, nmax, 0.5, 3
+        xmin, xmax, ymin, ymax = 0, nmax, 0.5, 5
         X = np.linspace(xmin, xmax,nx)[:,None] * np.ones((1,nx))
         Y = np.linspace(ymin, ymax,nx)[None,:] * np.ones((nx,1))
         XY = np.rollaxis(np.array([X,Y]), 0, 3)
@@ -37,15 +46,46 @@ class Potential:
         plt.contourf(X,Y,Z, np.round(np.linspace(np.min(Z), np.max(Z), 200), 2), cmap='RdGy')
         plt.colorbar()
 
-def makePlots(m, whichmins):    # whichmins is a list of positive integers, i.e. [1, 2, 3, 4] or [2]
+    def realmin(self, whichmin):
+        return root(nd.Gradient(self.V), whichmin).x
+
+    def cut(self, whichmin, coeff_ang):    #coeff_ang is between 0 and +infty
+        xcut = np.linspace(-1, whichmin*1.5, 1000)
+        real_min = self.realmin([whichmin,1.])
+        ycut = -coeff_ang*(xcut - real_min[0]*np.ones_like(xcut)) + real_min[1]
+        return xcut, ycut, self.V(np.array([xcut, ycut]).T)
+        
+
+def makePlots(m, whichmins, factor_steps):    # whichmins is a list of positive integers, i.e. [1, 2, 3, 4] or [2]
+    blockPrint()
+    
     plt.figure()
     m.plotContour()
 
     for whichmin in whichmins:
-        true, meta = [0.,1.], [whichmin,1.]
+        true_guess, meta_guess = [0.,1.], [whichmin,1.]
+        true, meta = m.realmin(true_guess), m.realmin(meta_guess)
+
         iniradius = (meta[0] - true[0])/2
         inipath_x = np.linspace(true[0], meta[0])
-        inipath_y =  true[1] + np.sqrt(-np.power(inipath_x - iniradius - true[0],2) + np.power(iniradius,2))
-        inipath = np.array([inipath_x, inipath_y]).T
-        Y = pd.fullTunneling(inipath, m.V, m.dV)
-        plt.plot(Y.Phi[:,0], Y.Phi[:,1], 'k', lw=1.5)
+
+        factor_range = np.linspace(0, 2, factor_steps, endpoint=True)
+        Y, Yposs = None, None
+        for factor in factor_range:
+            inipath_y = np.sqrt(-np.power(inipath_x - iniradius - true[0],2) + np.power(iniradius,2))
+            inipath_y = factor*inipath_y + true[1]
+            inipath = np.array([inipath_x, inipath_y]).T
+            try:
+                Yposs = pd.fullTunneling(inipath, m.V, m.dV, maxiter=2)
+            except Exception as e:
+                print(e)
+                pass
+
+            if Yposs is not None and Y is not None:
+                if Yposs.action < Y.action:
+                    Y = Yposs
+            elif Yposs is not None and Y is None:
+                Y = Yposs
+
+        if Y is not None:
+            plt.plot(Y.Phi[:,0], Y.Phi[:,1], 'k', lw=1.5)
--- tunneling.py
+++ tunneling.py
@@ -1,9 +1,17 @@
 # %%
+import numpy as np 
+import matplotlib.pyplot as plt
+from scipy.optimize import root
+from functools import partial
+import numdifftools as nd
 from bdahlen import makePlots, Potential
 import warnings
 warnings.filterwarnings("ignore")
 # %%
 if __name__ == '__main__':
-    V = Potential()
-    makePlots(V, [1,2,3,4])
+    m = Potential(a = 100, nmax = 3)
+    makePlots(m, [1, 2], 10)
+# %%
+    Vcut = m.cut(2, 0.)
+    plt.plot(Vcut[0],Vcut[2])
 # %%
