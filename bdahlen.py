a799af0712e574b55f7e2c2ca257a90d59a51435
gsalinasphys <gsalinas.phys@gmail.com>
2021-04-05 12:32:08 -0400

    Cut action

--- bdahlen.py
+++ bdahlen.py
@@ -1,11 +1,10 @@
+import sys, os
 import numpy as np 
+import numdifftools as nd
 import matplotlib.pyplot as plt
 from scipy.optimize import root
 from functools import partial
-import numdifftools as nd
 from cosmoTransitions import pathDeformation as pd
-
-import sys, os
 
 def blockPrint():
     sys.stdout = open(os.devnull, 'w')
@@ -14,18 +13,18 @@ def enablePrint():
     sys.stdout = sys.__stdout__
 
 class Potential:
-    def __init__(self, a = 50., E = 0., epsilon = .1, nmax = 5):
-        self.params = a, E, epsilon, nmax
+    def __init__(self, a = 50., E = 0., epsilon = .1, nmax = 5, whichmin = 2):
+        self.params = a, E, epsilon, nmax, whichmin
 
     def V(self, X):
         x,y = X[...,0], X[...,1]
-        a, E, epsilon, nmax = self.params
+        a, E, epsilon, nmax = self.params[:-1]
         somatory = sum([(1 - E - n*epsilon)*np.exp(-a*np.power(x - n, 2)) for n in np.arange(nmax)])
         return 1/np.power(y, 2) - np.exp(-a*np.power(y - 1, 2))*somatory
 
     def dV(self, X):
         x,y = X[...,0], X[...,1]
-        a, E, epsilon, nmax = self.params
+        a, E, epsilon, nmax = self.params[:-1]
         somatory = sum([(1 - E - n*epsilon)*np.exp(-a*np.power(x - n, 2)) for n in np.arange(nmax)])
         dsomatory = sum([(1 - E - n*epsilon)*np.exp(-a*np.power(x - n, 2))*(-2*a*(x - n)) for n in np.arange(nmax)])
         dVdx = - np.exp(-a*np.power(y - 1, 2))*dsomatory
@@ -37,7 +36,7 @@ class Potential:
 
     def plotContour(self):
         nx = 100
-        nmax = self.params[-1]
+        nmax = self.params[-2]
         xmin, xmax, ymin, ymax = 0, nmax, 0.5, 5
         X = np.linspace(xmin, xmax,nx)[:,None] * np.ones((1,nx))
         Y = np.linspace(ymin, ymax,nx)[None,:] * np.ones((nx,1))
@@ -46,46 +45,48 @@ class Potential:
         plt.contourf(X,Y,Z, np.round(np.linspace(np.min(Z), np.max(Z), 200), 2), cmap='RdGy')
         plt.colorbar()
 
-    def realmin(self, whichmin):
-        return root(nd.Gradient(self.V), whichmin).x
+    def realmin_meta(self):
+        whichmin = self.params[-1]
+        return root(nd.Gradient(self.V), [whichmin, 1.]).x
 
-    def cut(self, whichmin, coeff_ang):    #cut starts from 'whichmin', coeff_ang is between 0 and +infty
-        xcut = np.linspace(-1, whichmin*1.5, 1000)
-        real_min = self.realmin([whichmin,1.])
+    def realmin_zero(self):
+        return root(nd.Gradient(self.V), [0., 1.]).x
+
+    def cut(self, coeff_ang, xcut): #cut starts from 'whichmin', coeff_ang is between 0 and +infty
+        real_min = self.realmin_meta()
         ycut = -coeff_ang*(xcut - real_min[0]*np.ones_like(xcut)) + real_min[1]
-        return xcut, ycut, self.V(np.array([xcut, ycut]).T)
-        
+        return self.V(np.array([xcut, ycut]).T) 
 
-def makePlots(m, whichmins, factor_steps):    # whichmins is a list of positive integers, i.e. [1, 2, 3, 4] or [2]
-    blockPrint()
+def makePlot(m, factor_steps = 10):    # whichmins is a list of positive integers, i.e. [1, 2, 3, 4] or [2]
+    # blockPrint()
     
     plt.figure()
     m.plotContour()
 
-    for whichmin in whichmins:
-        true_guess, meta_guess = [0.,1.], [whichmin,1.]
-        true, meta = m.realmin(true_guess), m.realmin(meta_guess)
+    true, meta = m.realmin_zero(), m.realmin_meta()
 
-        iniradius = (meta[0] - true[0])/2
-        inipath_x = np.linspace(true[0], meta[0])
+    iniradius = (meta[0] - true[0])/2
+    inipath_x = np.linspace(true[0], meta[0])
 
-        factor_range = np.linspace(0, 2, factor_steps, endpoint=True)
-        Y, Yposs = None, None
-        for factor in factor_range:
-            inipath_y = np.sqrt(-np.power(inipath_x - iniradius - true[0],2) + np.power(iniradius,2))
-            inipath_y = factor*inipath_y + true[1]
-            inipath = np.array([inipath_x, inipath_y]).T
-            try:
-                Yposs = pd.fullTunneling(inipath, m.V, m.dV, maxiter=2)
-            except Exception as e:
-                print(e)
-                pass
+    factor_range = np.linspace(0, 2, factor_steps, endpoint=True)
+    Y, Yposs = None, None
+    for factor in factor_range:
+        inipath_y = np.sqrt(-np.power(inipath_x - iniradius - true[0],2) + np.power(iniradius,2))
+        inipath_y = factor*inipath_y + true[1]
+        inipath = np.array([inipath_x, inipath_y]).T
+        try:
+            Yposs = pd.fullTunneling(inipath, m.V, m.dV, maxiter=2)
+        except Exception as e:
+            enablePrint()
+            print(e)
+            blockPrint()
+            pass
 
-            if Yposs is not None and Y is not None:
-                if Yposs.action < Y.action:
-                    Y = Yposs
-            elif Yposs is not None and Y is None:
+        if Yposs is not None and Y is not None:
+            if Yposs.action < Y.action:
                 Y = Yposs
+        elif Yposs is not None and Y is None:
+            Y = Yposs
 
-        if Y is not None:
-            plt.plot(Y.Phi[:,0], Y.Phi[:,1], 'k', lw=1.5)
+    if Y is not None:
+        plt.plot(Y.Phi[:,0], Y.Phi[:,1], 'k', lw=1.5)
--- tunneling.py
+++ tunneling.py
@@ -1,17 +1,41 @@
 # %%
 import numpy as np 
 import matplotlib.pyplot as plt
-from scipy.optimize import root
-from functools import partial
 import numdifftools as nd
-from bdahlen import makePlots, Potential
 import warnings
+import time
+from scipy.optimize import root
+from scipy.signal import argrelextrema
+from sympy import Symbol
+from functools import partial
+from bdahlen import makePlot, Potential
+from cosmoTransitions.tunneling1D import SingleFieldInstanton
 warnings.filterwarnings("ignore")
 # %%
-if __name__ == '__main__':
-    m = Potential(a = 100, nmax = 3)
-    makePlots(m, [1, 2], 10)
+# x = Symbol('x')
+# def my_deriv(f):
+#     fsmp = f(x)
+#     return lambdify(x, fsmp.diff(x), 'numpy')
 # %%
-    Vcut = m.cut(2, 0.)
-    plt.plot(Vcut[0],Vcut[2])
+if __name__ == '__main__':
+    m = Potential(a = 50, nmax = 3, whichmin = 1)
+    makePlot(m)
+# %%
+    Vcut = lambda x: m.cut(0.0, x)
+    # Vcut = partial(m.cut, 0)
+    xplot = np.linspace(-1,3,1000)
+    Vplot = Vcut(xplot)
+    plt.plot(xplot, Vplot)
+# %%
+    whichmin = m.params[-1]
+    mins1d_index = argrelextrema(Vplot, np.less)
+    zero1d, meta1d = xplot[mins1d_index[0][0]], xplot[mins1d_index[0][whichmin]]
+    # s = SingleFieldInstanton(zero1d, meta1d, Vcut, nd.Derivative(Vcut))
+    # s = SingleFieldInstanton(zero1d, meta1d, Vcut, my_deriv(Vcut))
+    s = SingleFieldInstanton(zero1d, meta1d, Vcut)
+# %%
+    start = time.process_time()
+    pfl = s.findProfile()
+    s.findAction(pfl)
+    print((time.process_time() - start)/60.)
 # %%
